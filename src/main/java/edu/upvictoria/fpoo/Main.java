996ef17baf9bd262bbf31a6cd5274e7f3d41958f
RichardKobay <ricardo.roel110@gmail.com>
2024-01-24 22:59:20 -0600

    Finished the menu and it's placeholder for all the posible menu options

--- src/main/java/edu/upvictoria/fpoo/Main.java
+++ src/main/java/edu/upvictoria/fpoo/Main.java
@@ -1,12 +1,58 @@
 package edu.upvictoria.fpoo;
 
-// import edu.upvictoria.fpoo.EstructurasSecuenciales.EjercicioUno;
-// import edu.upvictoria.fpoo.*;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        Menu.printWelcome();
+        while (true) {
+            int opc = 0;
+            Menu.printMenu();
+            try {
+                opc = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println();
+                System.out.println();
+                System.out.println("----------------------------------");
+                System.out.println("---------OPCIÓN NO VÁLIDA---------");
+                System.out.println("--------INTÉNTALO DE NUEVO--------");
+                System.out.println("----------------------------------");
+                System.out.println();
+                System.out.println();
+                scanner.nextLine();
+            }
 
-        // EjercicioUno uno = new EjercicioUno();
+            if (opc >= 1 && opc <= 26) {
+                if (opc == 1) System.out.println("Option 1");
+                if (opc == 2) System.out.println("Option 2");
+                if (opc == 3) System.out.println("Option 3");
+                if (opc == 4) System.out.println("Option 4");
+                if (opc == 5) System.out.println("Option 5");
+                if (opc == 6) System.out.println("Option 6");
+                if (opc == 7) System.out.println("Option 7");
+                if (opc == 8) System.out.println("Option 8");
+                if (opc == 9) System.out.println("Option 9");
+                if (opc == 10) System.out.println("Option 10");
+                if (opc == 11) System.out.println("Option 11");
+                if (opc == 12) System.out.println("Option 12");
+                if (opc == 13) System.out.println("Option 13");
+                if (opc == 14) System.out.println("Option 14");
+                if (opc == 15) System.out.println("Option 15");
+                if (opc == 16) System.out.println("Option 16");
+                if (opc == 17) System.out.println("Option 17");
+                if (opc == 18) System.out.println("Option 18");
+                if (opc == 19) System.out.println("Option 19");
+                if (opc == 20) System.out.println("Option 20");
+                if (opc == 21) System.out.println("Option 21");
+                if (opc == 22) System.out.println("Option 22");
+                if (opc == 23) System.out.println("Option 23");
+                if (opc == 24) System.out.println("Option 24");
+                if (opc == 25) System.out.println("Option 25");
+                if (opc == 25) System.out.println("Option 26");
+            }
+        }
     }
 }
---
+++ src/main/java/edu/upvictoria/fpoo/Menu.java
@@ -0,0 +1,48 @@
+package edu.upvictoria.fpoo;
+
+public class Menu {
+
+    /**
+     * Prints the main menu
+     * */
+    public static void printMenu() {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("------------------Seleccione una opción------------------");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("1. Suma de dos números");
+        System.out.println("2. Cálculo de promedios de exámen");
+        System.out.println("3. Área de un rectángulo");
+        System.out.println("4. Área de una circunferencia");
+        System.out.println("5. Área de un terreno con forma irregular");
+        System.out.println("6. Área de la figura con forma de cono de nieve");
+        System.out.println("7. Producción de leche");
+        System.out.println("8. Distancia entre dos puntos en el plano cartesiano");
+        System.out.println("9. Sueldo semanal de un trabajador");
+        System.out.println("10. Modista");
+        System.out.println("11. Conagua");
+        System.out.println("12. Área de un triángulo");
+        System.out.println("13. Conversiones de peso a dolar");
+        System.out.println("14. Edad del personal");
+        System.out.println("15. Estacionamiento");
+        System.out.println("16. Pinturas");
+        System.out.println("17. Hipotenusa");
+        System.out.println("18. Autobuses \"La curva loca\"");
+        System.out.println("19. Tiempo en bicicleta");
+        System.out.println("20. Costo de llamada telefónica");
+        System.out.println("21. CONAGUA");
+        System.out.println("22. Compañía de luz y sombras CLS");
+        System.out.println("23. Descuento e IVA");
+        System.out.println("24. Ahorro");
+        System.out.println("25. Cheques");
+        System.out.println("26. Concepto de monto");
+    }
+
+    /**
+     * Prints the welcome menu
+     * */
+    public static void printWelcome() {
+        System.out.println("-------------------------------------");
+        System.out.println("----Bienvenido al problemario uno----");
+        System.out.println("-------------------------------------");
+    }
+}
