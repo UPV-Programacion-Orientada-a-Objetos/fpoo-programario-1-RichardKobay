7356737fb92c658ab2d2e587c5ab47833c8ab8b4
RichardKobay <ricardo.roel110@gmail.com>
2024-01-26 17:56:17 -0600

    Finished EjercicioVeintuno **NOT TESTED**

---
+++ src/main/java/edu/upvictoria/fpoo/EjercicioVeintuno.java
@@ -0,0 +1,28 @@
+package edu.upvictoria.fpoo;
+
+import edu.upvictoria.fpoo.math.Aritmethic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EjercicioVeintuno {
+    public EjercicioVeintuno () {
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+        Aritmethic aritmethic = new Aritmethic();
+
+        double kw = 0;
+        double price = 0;
+
+        try {
+            System.out.println("Give me the amount of kw");
+            kw = Double.parseDouble(scanner.readLine());
+            System.out.println("Give me the price per kw");
+            price = Double.parseDouble(scanner.readLine());
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("The input was not what we excepted");
+        }
+
+        System.out.println("The total price is: " + aritmethic.multiply(kw, price));
+    }
+}
