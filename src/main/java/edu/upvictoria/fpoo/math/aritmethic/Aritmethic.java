4ddd20642e483b4bb9fc1c55d32ebcf22c0cc329
RichardKobay <ricardo.roel110@gmail.com>
2024-01-26 11:50:58 -0600

    Added Aritmethic and Trigonometry classes to use in future excercises.

---
+++ src/main/java/edu/upvictoria/fpoo/math/aritmethic/Aritmethic.java
@@ -0,0 +1,13 @@
+package edu.upvictoria.fpoo.math.aritmethic;
+
+public class Aritmethic {
+    public int multiply(int a, int b) {return a * b;}
+    public double multiply(double a, double b) {return a * b;}
+    public float multiply(float a, float b) {return a * b;}
+    public int sum(int a, int b) {return a + b;}
+    public double sum(double a, double b) {return a + b;}
+    public float sum(float a, float b) {return a + b;}
+    public int divide(int a, int b) {return a / b;}
+    public double divide(double a, double b) {return a / b;}
+    public float divide(float a, float b) {return a / b;}
+}
---
+++ src/main/java/edu/upvictoria/fpoo/math/trigonometry/Trigonometry.java
@@ -0,0 +1,5 @@
+package edu.upvictoria.fpoo.math.trigonometry;
+
+public class Trigonometry {
+    public double getHypotenuse(int a, int b) {return Math.sqrt((a * a) + (b * b));}
+}
