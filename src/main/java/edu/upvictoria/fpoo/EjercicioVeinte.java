5169e6208811593c426b9e6997acb03931635dee
RichardKobay <ricardo.roel110@gmail.com>
2024-01-26 17:50:01 -0600

    Finished EjercicioVeinte **NOT TESTED**

---
+++ src/main/java/edu/upvictoria/fpoo/EjercicioVeinte.java
@@ -0,0 +1,28 @@
+package edu.upvictoria.fpoo;
+
+import edu.upvictoria.fpoo.math.Aritmethic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EjercicioVeinte {
+    public EjercicioVeinte() {
+        Aritmethic aritmethic = new Aritmethic();
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+
+        double price = 0;
+        double time = 0;
+
+        try {
+            System.out.println("Give me the time (minutes):");
+            time = Double.parseDouble(scanner.readLine());
+            System.out.println("Give me the price per minute:");
+            price = Double.parseDouble(scanner.readLine());
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("The input was not what we excepted");
+        }
+
+        System.out.println("The time will be: " + aritmethic.multiply(time, price));
+    }
+}
