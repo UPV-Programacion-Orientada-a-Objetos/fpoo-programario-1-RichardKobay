887f5eb8a341555e21ff7c55e7b90e30ab348219
RichardKobay <ricardo.roel110@gmail.com>
2024-01-26 18:10:47 -0600

    Finished EjercicioVeintidos and Fixed EjercicioVeintuno

--- src/main/java/edu/upvictoria/fpoo/EjercicioVeintidos.java
+++ src/main/java/edu/upvictoria/fpoo/EjercicioVeintidos.java
@@ -1,29 +1,28 @@
 package edu.upvictoria.fpoo;
 
-import edu.upvictoria.fpoo.helpers.Conagua;
 import edu.upvictoria.fpoo.math.Aritmethic;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class EjercicioVeintuno {
-    public EjercicioVeintuno () {
+public class EjercicioVeintidos {
+    public EjercicioVeintidos () {
         BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
-        Conagua conagua = new Conagua();
+        Aritmethic aritmethic = new Aritmethic();
 
-        double m3 = 0;
+        double kw = 0;
         double price = 0;
 
         try {
-            System.out.println("Give me the amount of m3");
-            m3 = Double.parseDouble(scanner.readLine());
-            System.out.println("Give me the price per m3");
+            System.out.println("Give me the amount of kw");
+            kw = Double.parseDouble(scanner.readLine());
+            System.out.println("Give me the price per kw");
             price = Double.parseDouble(scanner.readLine());
         } catch (IOException | NumberFormatException e) {
             System.out.println("The input was not what we excepted");
         }
 
-        System.out.println("The total price is: " + conagua.getTotalPerCubicMeter(m3, price));
+        System.out.println("The total price is: " + aritmethic.multiply(kw, price));
     }
 }
